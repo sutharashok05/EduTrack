07b98c91ed410207456969d2b18dba3792fea64f
sutharashok05 <ashoksuthar7849@gmail.com>
2026-01-20 09:48:29 +0530

    new changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 
